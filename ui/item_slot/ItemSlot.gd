5a9466070f4ab45a8a4d22a6a84fe73a62905e65
liamdyer <william.dyer@meraki.net>
2022-08-14 10:22:05 -0500

    Make item dropping more obvious

--- ui/item_slot/ItemSlot.gd
+++ ui/item_slot/ItemSlot.gd
@@ -1,7 +1,10 @@
 extends Control
 
+export(bool) var ground_slot
+
 onready var empty_texture = preload("res://images/ui/item_slot_empty.png")
 onready var filled_texture = preload("res://images/ui/item_slot.png")
+onready var filled_texture_drop = preload("res://images/ui/item_slot_drop.png")
 
 func clear():
 	$ItemStatsDisplay.visible = false
@@ -13,4 +16,7 @@ func set_item(item):
 		return
 	$ItemStatsDisplay.set_item(item)
 	$ItemStatsDisplay.visible = true
-	$SlotBackground.texture = filled_texture
+	if ground_slot:
+		$SlotBackground.texture = filled_texture_drop
+	else:
+		$SlotBackground.texture = filled_texture
